a98fbae9bbea91e38cd7fdd5b66067ebe6df1f69
samuelpswang <samuel.wang21@imperial.ac.uk>
2023-12-04 18:52:36 +0000

    feat: day 04 challenges

---
+++ day_04/part_1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+void parse_card(const string& filename, vector<unordered_set<int>>& win, vector<vector<int>>& you) {
+    ifstream ifs;
+    ifs.open(filename);
+    if (!ifs) exit(1);
+
+    string card, colon, sep, nextline;
+    int gameno;
+    while (ifs >> card >> gameno >> colon) {
+        unordered_set<int> w;
+        vector<int> y;
+        int num;
+        for (int i = 0; i < 10; i++) {
+            ifs >> num;
+            w.insert(num);
+        }
+        ifs >> sep;
+        for (int i = 0; i < 25; i++) {
+            ifs >> num;
+            y.push_back(num);
+        }
+        win.push_back(w);
+        you.push_back(y);
+    }
+}
+
+int find_winnings(vector<unordered_set<int>>& win, vector<vector<int>>& you) {
+    int winnings = 0;
+    for (int cid = 0; cid < win.size(); cid++) {
+        int count = 0;
+        for (int nid = 0; nid < you[cid].size(); nid++) {
+            if (win[cid].count(you[cid][nid])) count++;
+        }
+        winnings += pow(2, count-1);
+    }
+    return winnings;
+}
+
+int main() {
+    vector<unordered_set<int>> win;
+    vector<vector<int>> you;
+    parse_card("input.txt", win, you);
+    cout << find_winnings(win, you) << endl;
+}
---
+++ day_04/part_2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+void parse_card(const string& filename, vector<unordered_set<int>>& win, vector<vector<int>>& you) {
+    ifstream ifs;
+    ifs.open(filename);
+    if (!ifs) exit(1);
+
+    string card, colon, sep, nextline;
+    int gameno;
+    while (ifs >> card >> gameno >> colon) {
+        unordered_set<int> w;
+        vector<int> y;
+        int num;
+        for (int i = 0; i < 10; i++) {
+            ifs >> num;
+            w.insert(num);
+        }
+        ifs >> sep;
+        for (int i = 0; i < 25; i++) {
+            ifs >> num;
+            y.push_back(num);
+        }
+        win.push_back(w);
+        you.push_back(y);
+    }
+}
+
+void count_matches(vector<unordered_set<int>>& win, vector<vector<int>>& you, vector<vector<int>>& matches) {
+    for (int cid = 0; cid < win.size(); cid++) {
+        int count = 0;
+        for (int nid = 0; nid < you[cid].size(); nid++) {
+            if (win[cid].count(you[cid][nid])) count++;
+        }
+        matches.push_back({1, count});
+    }
+}
+
+int process_matches(vector<vector<int>>& matches) {
+    for (int cid = 0; cid < matches.size(); cid++) {
+        int copies = matches[cid][0];
+        for (int next = 1; next <= matches[cid][1]; next++) {
+            matches[cid+next][0] += copies;
+        }
+    }
+
+    int total = 0;
+    for (int cid = 0; cid < matches.size(); cid++) {
+        total += matches[cid][0];
+    }
+    return total;
+}
+
+
+int main() {
+    vector<unordered_set<int>> win;
+    vector<vector<int>> you;
+    parse_card("input.txt", win, you);
+
+    vector<vector<int>> matches;
+    count_matches(win, you, matches);
+
+    cout << process_matches(matches) << endl;
+}
